3050344888a877d60441008458fc6e0d98380d92
Doston Ismailov <dostonberuniy@gmail.com>
2023-08-17 14:54:29 +0500

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dostontiu/model-log',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '71524fa4f9bb46959668dbe9cad4e7a438ea2ed5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dostontiu/model-log' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '71524fa4f9bb46959668dbe9cad4e7a438ea2ed5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
